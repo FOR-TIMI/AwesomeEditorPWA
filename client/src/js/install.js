15d80735c5cc30d5c8db5c9a12625d8f11c41551
FOR-TIMI <timiplayskeys@gmail.com>
2022-11-08 22:31:23 -0500

    configured PWA to allow installation and sharing

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,40 @@
-const butInstall = document.getElementById('buttonInstall');
+const installButton = document.getElementById('buttonInstall');
+const shareButton = document.getElementById('buttonShare');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    installButton.style.visibility = 'visible';
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+    // click event handler on the `butInstall` element
+    installButton.addEventListener('click', ( ) => {
+        event.prompt();
+        installButton.ariaDisabled = true
+        installButton.textContent = 'Installed';
+    })
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+//Handler for share event
+shareButton.addEventListener('click', async() =>{
+
+    const shareData = {
+        title: 'JATE',
+        text: 'Just another text editor',
+      }
+
+    try {
+      shareData.url = window.location.href
+      await navigator.share(shareData);
+      console.log("👍👍👍 Text Editor app shared successfully")
+    } catch (err) {
+      console.error(err)
+    }
+
+})
+
+
+//Handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'J.A.T.E was installed successfully', event);
+});
